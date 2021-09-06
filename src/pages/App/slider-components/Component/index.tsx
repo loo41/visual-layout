3e11a03ca7df744e34b4d48d51a64e569ca35410
TCYong <18051702432qq.com>
2021-09-06 20:38:25 +0800

    feat(components): add componsnts search and tips

--- src/pages/App/slider-components/Component/index.tsx
+++ src/pages/App/slider-components/Component/index.tsx
@@ -1,6 +1,8 @@
+import { SearchOutlined } from '@ant-design/icons';
 import * as components from 'antd';
 import _ from 'lodash';
 import React, { useContext } from 'react';
+import { useState } from 'react';
 import { PagesContext } from 'src/context';
 import { PageService } from 'src/controller';
 import { EventType } from 'src/controller/browser';
@@ -9,6 +11,7 @@ import { ComponentsAST } from './const';
 import styles from './index.module.scss';
 
 const Components: React.FC<{}> = () => {
+  const [value, setValue] = useState('');
   const { pagesService } = useContext(PagesContext);
 
   pagesService.registerComponents(components);
@@ -17,9 +20,31 @@ const Components: React.FC<{}> = () => {
 
   return (
     <div className={styles.container}>
-      {ComponentsAST.map((ast, index) => (
-        <Component key={index} ast={ast} page={page} />
-      ))}
+      <div className={styles.search}>
+        <components.Input
+          placeholder="组件搜索"
+          onChange={e => setValue(e.target.value)}
+          addonAfter={<SearchOutlined />}
+        />
+      </div>
+      <div className={styles.components}>
+        {ComponentsAST.filter(component => {
+          return (
+            !value ||
+            !component.children ||
+            component.children.every(child => {
+              return (
+                !child.component ||
+                new RegExp(`${_.escapeRegExp(value)}`, 'ig').test(
+                  child.component?._name,
+                )
+              );
+            })
+          );
+        }).map((ast, index) => (
+          <Component key={index} ast={ast} page={page} />
+        ))}
+      </div>
     </div>
   );
 };
@@ -28,7 +53,11 @@ const Component: React.FC<{ ast: AST; page: PageService }> = ({ ast, page }) => 
   const node = page?.createNode(_.cloneDeep(ast));
   const DOM = node?.createElement({ eventType: EventType.layout });
 
-  return <div>{DOM}</div>;
+  return (
+    <components.Tooltip placement="right" title={ast.children[0]?.component?._name}>
+      <div>{DOM}</div>
+    </components.Tooltip>
+  );
 };
 
 export default Components;
