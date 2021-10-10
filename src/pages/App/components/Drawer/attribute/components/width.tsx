a303d79e5c34971647d1573a399abd00be81d709
markcytian <markcytian@tencent.com>
2021-10-10 20:29:28 +0800

    refactor(view): remove "vw" and "rem"

--- src/pages/App/components/Drawer/attribute/components/width.tsx
+++ src/pages/App/components/Drawer/attribute/components/width.tsx
@@ -16,8 +16,7 @@ const Width: React.FC<CssProps> = ({ style = [], onChange }) => {
   const width = style.filter(css => css.key === KEY)[0];
 
   useEffect(() => {
-    const [, pixel, unit] =
-      width?.value.trim().match(/^([0-9]*)(%|px|rem|em)/) || [];
+    const [, pixel, unit] = width?.value.trim().match(/^([0-9]*)(%|px)/) || [];
     setOption(unit || 'px');
     setValue(pixel || '');
     unitValue.current = {
@@ -50,20 +49,18 @@ const Width: React.FC<CssProps> = ({ style = [], onChange }) => {
             onChange={unit => {
               setOption(unit);
               setValue(
-                ['rem', 'custom', 'vw'].includes(unit)
+                ['custom'].includes(unit)
                   ? '暂不支持'
                   : unitValue.current?.[unit] || '',
               );
             }}
           >
             <Option value="px">px</Option>
-            <Option value="rem">rem</Option>
-            <Option value="vw">vw</Option>
             <Option value="%">%</Option>
             <Option value="custom">自定义</Option>
           </Select>
         }
-        disabled={['rem', 'custom', 'vw'].includes(option)}
+        disabled={['custom'].includes(option)}
         onBlur={() => setStyle()}
         value={value}
         onChange={e => setValue(e.target.value)}
